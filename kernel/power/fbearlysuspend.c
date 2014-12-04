79ec5846d49bd9c731f547bae81d24777dd1e2a4
Роман <romangav99999@gmail.com>
2014-12-04 14:19:35 +0200

    Update fbearlysuspend.c
    Signed-off-by: Anonim47 <yosefadis@hotmail.com>

--- kernel/power/fbearlysuspend.c
+++ kernel/power/fbearlysuspend.c
@@ -16,6 +16,7 @@
 #include <linux/earlysuspend.h>
 #include <linux/module.h>
 #include <linux/wait.h>
+#include <linux/delay.h>
 
 #include "power.h"
 
@@ -30,12 +31,15 @@ static enum {
 	FB_STATE_DRAWING_OK,
 } fb_state;
 
+int fbearlysuspend_delay = 100;
+
 /* tell userspace to stop drawing, wait for it to stop */
 static void stop_drawing_early_suspend(struct early_suspend *h)
 {
 	int ret;
 	unsigned long irq_flags;
 
+	msleep(fbearlysuspend_delay); 
 	spin_lock_irqsave(&fb_state_lock, irq_flags);
 	fb_state = FB_STATE_REQUEST_STOP_DRAWING;
 	spin_unlock_irqrestore(&fb_state_lock, irq_flags);
