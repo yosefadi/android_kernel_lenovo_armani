8c6efeb32fa77c6fba277c2c40778a27e14b0ebc
Anonim47 <yosefadis@hotmail.com>
2015-07-04 22:55:14 +0700

    Voltage Regulating: My own twist!
    MSM8x25Q should be run better  in battery life!
    Signed-off-by: Anonim47 <yosefadis@hotmail.com>

--- arch/arm/mach-msm/acpuclock-8625q.c
+++ arch/arm/mach-msm/acpuclock-8625q.c
@@ -120,10 +120,10 @@ static struct clock_state drv_state = { 0 };
 
 /* PVS MAX Voltage in uV as per frequencies*/
 
-# define MAX_14GHZ_VOLTAGE 1350000
-# define MAX_12GHZ_VOLTAGE 1275000
-# define MAX_1GHZ_VOLTAGE 1175000
-# define MAX_NOMINAL_VOLTAGE 1150000
+# define MAX_14GHZ_VOLTAGE 1270000
+# define MAX_12GHZ_VOLTAGE 1198000
+# define MAX_1GHZ_VOLTAGE 1125000
+# define MAX_NOMINAL_VOLTAGE 1095000
 
 /* PVS deltas as per formula*/
 # define DELTA_LEVEL_1_UV 0
