afb98dbb9b01d428f01baf503d13310cbf49e103
Anonim47 <yosefadis@hotmail.com>
2015-07-06 12:43:41 +0700

    Revert "msm_fb: display: exit properly when rotator max session reached"
    This reverts commit 65fa33f38d005537c3a06a06ea470ab8233d4909.

--- drivers/char/msm_rotator.c
+++ drivers/char/msm_rotator.c
@@ -941,9 +941,7 @@ static int msm_rotator_do_rotate(unsigned long arg)
 		pr_err("%s() : Attempt to use invalid session_id %d\n",
 			__func__, s);
 		rc = -EINVAL;
-		//goto do_rotate_unlock_mutex;
-		mutex_unlock(&msm_rotator_dev->rotator_lock);
-		return rc;
+		goto do_rotate_unlock_mutex;
 	}
 
 	if (msm_rotator_dev->img_info[s]->enable == 0) {
@@ -951,9 +949,7 @@ static int msm_rotator_do_rotate(unsigned long arg)
 			"%s() : Session_id %d not enabled \n",
 			__func__, s);
 		rc = -EINVAL;
-		//goto do_rotate_unlock_mutex;
-		mutex_unlock(&msm_rotator_dev->rotator_lock);
-		return rc;
+		goto do_rotate_unlock_mutex;
 	}
 
 	img_info = msm_rotator_dev->img_info[s];
@@ -963,9 +959,7 @@ static int msm_rotator_do_rotate(unsigned long arg)
 					&src_planes)) {
 		pr_err("%s: invalid src format\n", __func__);
 		rc = -EINVAL;
-		//goto do_rotate_unlock_mutex;
-		mutex_unlock(&msm_rotator_dev->rotator_lock);
-		return rc;
+		goto do_rotate_unlock_mutex;
 	}
 	if (msm_rotator_get_plane_sizes(img_info->dst.format,
 					img_info->dst.width,
@@ -973,9 +967,7 @@ static int msm_rotator_do_rotate(unsigned long arg)
 					&dst_planes)) {
 		pr_err("%s: invalid dst format\n", __func__);
 		rc = -EINVAL;
-		//goto do_rotate_unlock_mutex;
-		mutex_unlock(&msm_rotator_dev->rotator_lock);
-		return rc;
+		goto do_rotate_unlock_mutex;
 	}
 
 	rc = get_img(&info.src, ROTATOR_SRC_DOMAIN, (unsigned long *)&in_paddr,
